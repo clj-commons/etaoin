43ce84d4a7120a2edac59fbe102651d7025054cf
Ivan Grishaev <ivan@grishaev.me>
2017-02-10 18:39:02 +0300

    project refactored

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to webdriver
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject webdriver "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Pure Clojure Webdriver protocol implementation."
+  :url "https://github.com/igrishaev/webdriver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.3.0"]
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
+                                  [org.clojure/tools.nrepl "0.2.5"]]}}
+
+  :dependencies [[clj-http "2.3.0"]
                  [cheshire "5.6.3"]
-                 [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/tools.logging "0.3.1"]])
+                 [org.clojure/data.codec "0.1.0"]])
