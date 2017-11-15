cd8e2973e525bd3f8203d2edda7fdb90613af0de
Ivan Grishaev <ivan@grishaev.me>
2017-11-15 17:40:00 +0300

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject etaoin "0.1.9-SNAPSHOT"
+(defproject etaoin "0.2.0"
   :description "Pure Clojure Webdriver protocol implementation."
   :url "https://github.com/igrishaev/etaoin"
   :license {:name "Eclipse Public License"
