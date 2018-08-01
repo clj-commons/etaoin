7ab96694b75b225dca3ae924e119c0c1dfde7431
Marco Molteni <marco.molteni@laposte.net>
2018-08-01 18:14:03 +0200

    Collect test run information on CircleCI
    When running the tests on CicleCI, emit XUNIT test reports so
    that CircleCI call collect and show test statistics.
    
    Tests statistics are shown in two places:
    - each single build gives an overview of the failing tests
    - the build-insights page at
      https://circleci.com/build-insights/gh/igrishaev/etaoin/master

--- project.clj
+++ project.clj
@@ -17,6 +17,11 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/data.codec "0.1.0"]]
 
+  ; When running the tests as `lein test2junit`, emit XUNIT test reports to enable CircleCI
+  ; to collect statistics over time
+  :plugins [[test2junit "1.1.2"]]
+  :test2junit-output-dir "target/test2junit"
+
   :autodoc {:name "Etaoin"
             :page-title "Etaoin API Documentation"
             :description "Pure Clojure Webdriver protocol implementation."
