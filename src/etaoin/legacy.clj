2d26401ed19d762ac1841d9f831c8f52f046e13e
Alex.Shi <uunnamed@mail.ru>
2020-07-22 13:22:53 +0300

    better legacy code (#238)

--- src/etaoin/legacy.clj
+++ src/etaoin/legacy.clj
@@ -9,34 +9,20 @@
   https://stackoverflow.com/questions/27343707"
   (:require [clojure.string]))
 
-(defmacro with-ns
-  [ns & body]
-  `(binding [*ns* (the-ns ~ns)]
-     ~@(map (fn [form] `(eval '~form)) body)))
+(defn intern-fq [fqsym val]
+  (when-not (resolve fqsym)
+    (intern (-> fqsym namespace symbol)
+            (-> fqsym name symbol)
+            val)))
 
-(when-not (resolve 'clojure.string/starts-with?)
-  (with-ns 'clojure.string
+(intern-fq 'clojure.string/starts-with?
+           (fn [^CharSequence s ^String substr]
+             (.startsWith (.toString s) substr)))
 
-    (defn starts-with?
-      "True if s starts with substr."
-      {:added "etaoin.legacy"}
-      [^CharSequence s ^String substr]
-      (.startsWith (.toString s) substr))))
+(intern-fq 'clojure.string/ends-with?
+           (fn [^CharSequence s ^String substr]
+             (.endsWith (.toString s) substr)))
 
-(when-not (resolve 'clojure.string/ends-with?)
-  (with-ns 'clojure.string
-
-    (defn ends-with?
-      "True if s ends with substr."
-      {:added "etaoin.legacy"}
-      [^CharSequence s ^String substr]
-      (.endsWith (.toString s) substr))))
-
-(when-not (resolve 'clojure.string/includes?)
-  (with-ns 'clojure.string
-
-    (defn includes?
-      "True if s includes substr."
-      {:added "etaoin.legacy"}
-      [^CharSequence s ^CharSequence substr]
-      (.contains (.toString s) substr))))
+(intern-fq 'clojure.string/includes?
+           (fn [^CharSequence s ^CharSequence substr]
+             (.contains (.toString s) substr)))
