0c90a331ba8faa32d19f1d56b12574ad6923b667
Ivan Grishaev <ivan@grishaev.me>
2020-07-23 10:07:16 +0300

    Version 0.3.7

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
                   ["vcs" "tag" "--no-sign"]
-                  ["deploy" "clojars"]
+                  ["deploy"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
                   ["vcs" "push"]
