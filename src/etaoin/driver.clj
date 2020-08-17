faf173b0c9cf5828b2d2688ef14e64830a333612
Uunnamed <uunnamed@mail.ru>
2020-08-17 15:11:25 +0300

    update

--- src/etaoin/driver.clj
+++ src/etaoin/driver.clj
@@ -159,11 +159,12 @@
   ;; Chrome adds the trailing `/Default` part to the profile path.
   ;; To prevent duplication, let's clear the given path manually.
   [driver profile]
-  (let [default       #"(\\|/)Default$"
-        profile-path  (string/replace profile default "")
-        splited-p     (string/split profile-path #"/")
-        user-data-dir (string/join "/" (drop-last splited-p))
-        profile-dir   (last splited-p)]
+  (let [profile       (java.io.File. profile)
+        profile       (if (= "Default" (.getName profile))
+                        (.getParent profile)
+                        profile)
+        user-data-dir (str (.getParent profile))
+        profile-dir   (str  (.getName profile))]
     (set-options-args driver [(format "--user-data-dir=%s" user-data-dir)
                               (format "--profile-directory=%s" profile-dir)])))
 
