c338a571dc597035203ae85cf8a3d24baeec0543
Uunnamed <uunnamed@mail.ru>
2020-08-24 18:38:20 +0300

    update

--- test/etaoin/proc_test.clj
+++ test/etaoin/proc_test.clj
@@ -2,13 +2,22 @@
   (:require [etaoin.api :refer :all]
             [clojure.java.shell :refer [sh]]
             [clojure.test :refer :all]
-            [etaoin.proc :as proc]))
+            [etaoin.proc :as proc]
+            [clojure.string :as str]))
+(defn get-count-chromedriver-processes
+  []
+  (->> (sh "sh" "-c" "ps aux | grep chromedriver")
+       :out
+       str/split-lines
+       (drop-last 2)
+       count))
 
 (deftest test-prevent-process-fork
   (testing "certain driver port"
     (let [port    9999
           process (proc/run ["chromedriver" (format "--port=%d" port)])
           _       (wait-running (atom {:port port :host "localhost"}))]
+      (is (= 1 (get-count-chromedriver-processes)))
       (is (thrown-with-msg?
             clojure.lang.ExceptionInfo
             #"already in use"
@@ -19,5 +28,12 @@
           process (proc/run ["chromedriver" (format "--port=%d" port)])
           _       (wait-running (atom {:port port :host "localhost"}))]
       (with-chrome {:args ["--no-sandbox"]} driver
-        (is true "driver runnig")
-        (proc/kill process)))))
+        (is (= 2 (get-count-chromedriver-processes)))
+        (proc/kill process))))
+  (testing "connect to driver"
+    (let [port    9999
+          process (proc/run ["chromedriver" (format "--port=%d" port)])
+          _       (wait-running (atom {:port port :host "localhost"}))
+          driver  (chrome {:host "localhost" :port port :args ["--no-sandbox"]})]
+      (is (= 1 (get-count-chromedriver-processes)))
+      (proc/kill process))))
