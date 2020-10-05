220208ed770431c4aaab3154ff8aed3b77bf7c69
Uunnamed <uunnamed@mail.ru>
2020-10-05 16:37:06 +0300

    split ide stuff into a package

---
+++ src/etaoin/ide/flow.clj
@@ -0,0 +1,120 @@
+(ns etaoin.ide.flow
+  (:require [cheshire.core :refer [parse-stream]]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [etaoin.api :refer :all]
+            [etaoin.ide.api :refer [run-command-with-log str->var]]))
+
+(declare execute-commands)
+
+(defn execute-branch
+  [driver {:keys [this branch]} opt]
+  (when (run-command-with-log driver this opt)
+    (execute-commands driver branch opt)
+    true))
+
+(defn execute-if
+  [driver {:keys [if else-if else end]} opt]
+  (or (execute-branch driver if opt)
+      (some #(execute-branch driver % opt) else-if)
+      (execute-commands driver (:branch else) opt))
+  (run-command-with-log driver end opt))
+
+(defn execute-times
+  [driver {:keys [this branch end]} opt]
+  (let [n (run-command-with-log driver this opt)]
+    (doseq [commands (repeat n branch)]
+      (execute-commands driver commands opt))
+    (run-command-with-log driver end opt)))
+
+(defn execute-do
+  [driver {:keys [this branch repeat-if]} opt]
+  (run-command-with-log driver this opt)
+  (loop [commands branch]
+    (execute-commands driver commands opt)
+    (when (run-command-with-log driver repeat-if opt)
+      (recur commands))))
+
+(defn execute-while
+  [driver {:keys [this branch end]} opt]
+  (while (run-command-with-log driver this opt)
+    (execute-commands driver branch opt))
+  (run-command-with-log driver end opt))
+
+(defn execute-for-each
+  [driver {:keys [this branch end]} {vars :vars :as opt}]
+  (let [[var-name arr] (run-command-with-log driver this opt)]
+    (doseq [val arr]
+      (swap! vars assoc var-name val)
+      (execute-commands driver branch opt))
+    (run-command-with-log driver end opt)))
+
+(defn execute-cmd-with-open-window
+  [driver {:keys [windowHandleName windowTimeout] :as cmd} {vars :vars :as opt}]
+  (let [init-handles  (set (get-window-handles driver))
+        _             (run-command-with-log driver cmd opt)
+        _             (wait (/ windowTimeout 1000))
+        final-handles (set (get-window-handles driver))
+        handle        (first (clojure.set/difference final-handles init-handles))]
+    (swap! vars assoc (str->var windowHandleName) handle)))
+
+(defn execute-commands
+  [driver commands opt]
+  (doseq [[cmd-name cmd] commands]
+    (case cmd-name
+      :if                   (execute-if driver cmd opt)
+      :times                (execute-times driver cmd opt)
+      :do                   (execute-do driver cmd opt)
+      :while                (execute-while driver cmd opt)
+      :for-each             (execute-for-each driver cmd opt)
+      :cmd-with-open-window (execute-cmd-with-open-window driver cmd opt)
+      :cmd                  (run-command-with-log driver cmd opt)
+      (throw (ex-info "Command is not valid" {:command cmd})))))
+
+(defn run-ide-test
+  [driver {:keys [commands]} & [opt]]
+  (let [command->kw   (fn [{:keys [command] :as cmd}]
+                        (assoc cmd :command (keyword command)))
+        commands      (map command->kw commands)
+        commands-tree (s/conform :etaoin.ide.spec/commands commands)]
+    (when (s/invalid?  commands-tree)
+      (throw (ex-info "Incomplete or invalid command in the config"
+                      {:explain-data (s/explain-data :etaoin.ide.spec/commands commands)})))
+    (execute-commands driver commands-tree opt)))
+
+(defn get-tests-by-suite-id
+  [suite-id id {:keys [suites tests]}]
+  (let [test-ids    (-> (filter #(= suite-id (id %)) suites)
+                        first
+                        :tests
+                        set)
+        suite-tests (filter #(test-ids (:id %)) tests)]
+    suite-tests))
+
+(defn find-tests
+  [{:keys [test-id test-ids suite-id suite-ids test-name suite-name]}
+   {:keys [tests] :as parsed-file}]
+  (let [test-ids    (cond-> #{}
+                      test-id    (conj (first (filter #(= test-id (:id %)) tests)))
+                      test-name  (conj (first (filter #(= test-name (:name %)) tests)))
+                      suite-id   (into (get-tests-by-suite-id suite-id :id parsed-file))
+                      suite-name (into (get-tests-by-suite-id suite-name :name parsed-file))
+                      test-ids   (into (filter #((set test-ids) (:id %)) tests))
+                      suite-ids  (into (mapcat #(get-tests-by-suite-id % :id parsed-file) suite-ids)))
+        tests-found (filter test-ids tests)]
+    (if (empty? tests-found)
+      tests
+      tests-found)))
+
+(defn run-ide-script
+  [driver path & [opt]]
+  (let [parsed-file (with-open [rdr (io/reader path)]
+                      (parse-stream rdr true))
+        opt-search  (select-keys opt [:test-name :test-id :test-ids
+                                      :suite-name :suite-id :suite-ids])
+        tests-found (find-tests opt-search parsed-file)
+        opt         (merge {:base-url (:url parsed-file)
+                            :vars     (atom {})}
+                           opt)]
+    (doseq [test tests-found]
+      (run-ide-test driver test opt))))
---
+++ src/etaoin/ide/main.clj
@@ -0,0 +1,8 @@
+(ns etaoin.ide.main
+  (:require [etaoin.api :refer :all]
+            [etaoin.ide.flow :refer [run-ide-script]])
+  (:gen-class))
+
+
+(defn -main
+  [& args])
