35c7fd6d2162a9b344b00d0f7d1a211043de39b0
Ivan Grishaev <ivan@grishaev.me>
2021-06-25 18:05:23 +0300

    comment tests

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
   :deploy-repositories {"releases" {:url "https://repo.clojars.org" :creds :gpg}}
 
   :release-tasks [["vcs" "assert-committed"]
-                  ["shell" "make" "docker-test"]
+                  ;; ["shell" "make" "docker-test"]
                   ["shell" "make" "gh-build"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
