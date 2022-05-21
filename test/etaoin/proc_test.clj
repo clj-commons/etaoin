3c715619cc8381ffcaa984fcebeca46070674515
lread <lee@dlread.com>
2022-05-21 15:27:27 -0400

    more Windows proc flaky diagnosis

--- test/etaoin/proc_test.clj
+++ test/etaoin/proc_test.clj
@@ -12,7 +12,12 @@
     (let [instance-report (-> (sh "powershell" "-command" "(Get-Process chromedriver -ErrorAction SilentlyContinue).Path")
                               :out
                               str/split-lines)]
+      ;; more flakiness diagnosis
       (println "windows chromedriver instance report:" instance-report)
+      (println "windows full list of running processes:")
+      (clojure.pprint/pprint (-> (sh "powershell" "-command" "Get-Process | Select Name, Path")
+                                 :out
+                                 str/split-lines))
       (->> instance-report
            (remove #(str/includes? % "\\scoop\\shims\\")) ;; for the scoop users, exclude the shim process
            (filter #(str/includes? % "chromedriver"))
@@ -23,33 +28,34 @@
          (filter #(str/includes? % "chromedriver"))
          count)))
 
-(deftest test-prevent-process-fork
-  (testing "certain driver port"
-    (let [port    9999
-          process (proc/run ["chromedriver" (format "--port=%d" port)])
-          _       (wait-running {:port port :host "localhost"})]
-      (is (= 1 (get-count-chromedriver-instances)))
-      (is (thrown-with-msg?
-            clojure.lang.ExceptionInfo
-            #"already in use"
-            (chrome {:port port})))
-      (proc/kill process)))
-  (testing "random driver port"
-    (let [port    9999
-          process (proc/run ["chromedriver" (format "--port=%d" port)])
-          _       (wait-running {:port port :host "localhost"})]
-      (with-chrome {:args ["--no-sandbox"]} driver
+(deftest test-process-forking-port-specified
+  (let [port    9999
+        process (proc/run ["chromedriver" (format "--port=%d" port)])
+        _       (wait-running {:port port :host "localhost"})]
+    (is (= 1 (get-count-chromedriver-instances)))
+    (is (thrown-with-msg?
+         clojure.lang.ExceptionInfo
+         #"already in use"
+         (chrome {:port port})))
+    (proc/kill process)))
+
+(deftest test-process-forking-port-random
+  (let [port    9999
+        process (proc/run ["chromedriver" (format "--port=%d" port)])
+        _       (wait-running {:port port :host "localhost"})]
+    (with-chrome {:args ["--no-sandbox"]} driver
         ;; added to diagnose flakyness on windows on CI
-        (println "automatically chosen port->" (:port driver))
+      (println "automatically chosen port->" (:port driver))
         ;; added to diagnose flakyness on windows on CI
-        (wait-running {:port (:port driver) :host "localhost"})
-        (is (= 2 (get-count-chromedriver-instances))))
-      (proc/kill process)))
-  (testing "connect to driver"
-    (let [port    9999
-          process (proc/run ["chromedriver" (format "--port=%d" port)])
-          _       (wait-running {:port port :host "localhost"})
-          driver  (chrome {:host "localhost" :port port :args ["--no-sandbox"]})]
-      (is (= 1 (get-count-chromedriver-instances)))
-      (quit driver)
-      (proc/kill process))))
+      (wait-running {:port (:port driver) :host "localhost"})
+      (is (= 2 (get-count-chromedriver-instances))))
+    (proc/kill process)))
+
+(deftest test-process-forking-connect-existing
+  (let [port    9999
+        process (proc/run ["chromedriver" (format "--port=%d" port)])
+        _       (wait-running {:port port :host "localhost"})
+        driver  (chrome {:host "localhost" :port port :args ["--no-sandbox"]})]
+    (is (= 1 (get-count-chromedriver-instances)))
+    (quit driver)
+    (proc/kill process)))
