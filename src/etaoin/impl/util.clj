379e47fd66edc7057cb7d8e8fe94acba5d32134b
lread <lee@dlread.com>
2022-07-01 12:26:47 -0400

    Driver defaults :path renamed to :path-driver
    Also:
    - no longer add nil driver map values
    - add `defaults-global` for defaults that apply to all driver types
    - add some more tests around created driver map
    - localized handling of most driver option defaults to one spot
    
    Closes #471

--- src/etaoin/impl/util.clj
+++ src/etaoin/impl/util.clj
@@ -82,3 +82,13 @@
         (.getPort u)
         (.getFile u)
         (.getRef u)))))
+
+(defn assoc-some
+  "Associates a key with a value in a map, if and only if the value is
+  not nil. From medley."
+  ([m k v]
+   (if (nil? v) m (assoc m k v)))
+  ([m k v & kvs]
+   (reduce (fn [m [k v]] (assoc-some m k v))
+           (assoc-some m k v)
+           (partition 2 kvs))))
