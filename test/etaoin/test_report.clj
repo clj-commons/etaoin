486d80ae01059f91db35dfd10e937f2bf12242a9
Lee Read <lee@dlread.com>
2024-03-17 12:54:44 -0400

    maint: bump deps to current (#564)
    Of note:
    - address new clj-kondo findings (dynamic tests vars were uninitialized).

--- test/etaoin/test_report.clj
+++ test/etaoin/test_report.clj
@@ -1,13 +1,13 @@
 (ns etaoin.test-report
   (:require [clojure.test]))
 
-(def ^:dynamic *context*)
+(def ^:dynamic *context* nil)
 
 (def platform
   (if (System/getProperty "babashka.version") "bb" "jvm"))
 
 (defmethod clojure.test/report :begin-test-var [m]
   (let [test-name (-> m :var meta :name)]
-    (if (bound? #'*context*)
+    (if *context*
       (println (format "=== %s [%s][%s]" test-name platform *context*))
       (println (format "=== %s [%s]" test-name platform)))))
