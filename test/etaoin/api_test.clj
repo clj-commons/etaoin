7c9bbea27a2370bcb9a9bfb1d007af23f718cfdc
Lee Read <lee@dlread.com>
2025-04-18 20:28:59 +0000

    Don't swallow exceptions from parsing json (#697)
    * Don't swallow exceptions from parsing json
    
    When Etaoin failed parse a json webdriver response, it would swallow the
    exception. This lead to confusing "empty page" exceptions.
    
    We now let the true json parse exception to flow through.
    
    Closes #696
    
    * omit safari from json parse error test

--- test/etaoin/api_test.clj
+++ test/etaoin/api_test.clj
@@ -3,6 +3,7 @@
    [babashka.fs :as fs]
    [babashka.process :as p]
    [cheshire.core :as json]
+   [cheshire.factory :as cheshire-factory]
    [clojure.edn :as edn]
    [clojure.java.io :as io]
    [clojure.java.shell :as shell]
@@ -1332,6 +1333,19 @@
             true))
     (is (do (e/set-page-load-timeout *driver* 100/3)
             true))))
+
+(deftest test-json-parse-error
+  ;; as of 2025-04-18 the safari driver does not support print page,
+  ;; that's OK this test is less driver specific and more json parse specific
+  (e/when-not-safari *driver*
+    ;; output pdf should never be created, but just in case...
+    (let [out-pdf (-> (fs/create-temp-file {:prefix "print-page" :suffix "pdf"})
+                      fs/delete-on-exit)]
+      (is (thrown-with-msg?
+            Exception #"(?i)string.*exceeds.*512"
+            (binding [cheshire-factory/*json-factory* (cheshire-factory/make-json-factory
+                                                        {:max-input-string-length 512})]
+              (e/print-page *driver* out-pdf)))))))
 
 (comment
   ;; start test server
